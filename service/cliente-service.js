b49ac57e8c673ccaeaca06cb96203df60295dcef
Kaio <kaioramos990@hotmail.com>
2023-07-20 00:47:36 -0300

    Add cadastro de usuários CREATE RUD

---
+++ service/cliente-service.js
@@ -0,0 +1,37 @@
+const listaClientes = () =>  {
+    return fetch(`http://localhost:3000/profile`)
+    .then(resposta => {
+        return resposta.json()
+    })
+}
+
+const criaCliente = (nome, email) => { 
+    return fetch(`http://localhost:3000/profile`, {
+        method: 'POST', 
+        //method post -> de postar
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            //stringify para transformar em texto
+            nome: nome,
+            email: email
+        })
+    })
+    .then( resposta => {
+        return resposta.body
+    })
+}
+
+// const removeCliente = (id) => { 
+//     return fetch(`http://localhost:3000/profile/${id}`, {
+//         method: 'DELETE'
+//     })
+// }
+
+export const clienteService = { 
+    listaClientes,
+    criaCliente
+}
+
+//exportando objetos que são funções para outros arquivos
